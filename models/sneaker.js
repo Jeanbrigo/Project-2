ad657414fb942deb1b1327b5be693121d126e50d
Jean Brigonnet <jeanbrigo@gmail.com>
2022-12-08 07:54:33 -0600

    made sneaker model

---
+++ models/sneaker.js
@@ -0,0 +1,20 @@
+////////////////////////////////////////////////
+// Our Models
+////////////////////////////////////////////////
+// pull schema and model from mongoose
+const mongoose = require('./connection') // we imported mongoose here
+
+const { Schema, model } = mongoose // destructuring, grabbing model and schema off mongoose variable
+
+// make Sneakers schema
+const sneakerSchema = new Schema({
+  model: String,
+  image: String,
+  highTop: Boolean,
+  username: String,
+})
+
+// make fruit model
+const Sneaker = model("Sneaker", sneakerSchema)
+
+module.exports = Sneaker
